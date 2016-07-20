62a37607e27c17fa20daa1d11122ce39ec49a404
John Ky <newhoggy@gmail.com>
2016-07-21 08:38:09 +1000

    FromForeignRegion instances for vectors

--- src/HaskellWorks/Data/FromForeignRegion.hs
+++ src/HaskellWorks/Data/FromForeignRegion.hs
@@ -1,9 +1,32 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module HaskellWorks.Data.FromForeignRegion where
 
+import qualified Data.ByteString          as BS
+import qualified Data.ByteString.Internal as BSI
+import qualified Data.Vector.Storable     as DVS
 import           Data.Word
 import           Foreign.ForeignPtr
+import           HaskellWorks.Data.FromByteString
+
+type ForeignRegion = (ForeignPtr Word8, Int, Int)
 
 -- | Class for datastructures that can be created from a foreign region
 class FromForeignRegion a where
   -- | Create a value of type @a from a foreign region.
-  fromForeignRegion :: (ForeignPtr Word8, Int, Int) -> a
+  fromForeignRegion :: ForeignRegion -> a
+
+instance FromForeignRegion BS.ByteString where
+  fromForeignRegion (fptr, offset, size) = BSI.fromForeignPtr (castForeignPtr fptr) offset size
+
+instance FromForeignRegion (DVS.Vector Word8) where
+  fromForeignRegion = fromByteString . fromForeignRegion
+
+instance FromForeignRegion (DVS.Vector Word16) where
+  fromForeignRegion = fromByteString . fromForeignRegion
+
+instance FromForeignRegion (DVS.Vector Word32) where
+  fromForeignRegion = fromByteString . fromForeignRegion
+
+instance FromForeignRegion (DVS.Vector Word64) where
+  fromForeignRegion = fromByteString . fromForeignRegion

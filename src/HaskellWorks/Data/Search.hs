92573e7c8dcfbdb48b24e930bd19b91bb798cae4
John Ky <newhoggy@gmail.com>
2016-07-24 22:23:48 +1000

    Fix binarySearch

--- src/HaskellWorks/Data/Search.hs
+++ src/HaskellWorks/Data/Search.hs
@@ -7,7 +7,7 @@ module HaskellWorks.Data.Search
 binarySearch :: (Ord a, Integral n) => a -> (n -> a) -> n -> n -> n
 binarySearch w f p q = if p + 1 >= q
   then p
-  else let m = p + q `div` 2 in
+  else let m = (p + q) `div` 2 in
     if w <= f m
       then binarySearch w f p m
       else binarySearch w f m q
--- test/HaskellWorks/Data/SearchSpec.hs
+++ test/HaskellWorks/Data/SearchSpec.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module HaskellWorks.Data.SearchSpec (spec) where
 
+import           HaskellWorks.Data.Search
 import           Test.Hspec
 
-{-# ANN module ("HLint: ignore Redundant do" :: String) #-}
+{-# ANN module ("HLint: Ignore Redundant do" :: String) #-}
 
 spec :: Spec
-spec = describe "HaskellWorks.Data.BitReadSpec" $ do
+spec = describe "HaskellWorks.Data.SearchSpec" $ do
   it "No tests" $ do
-    "" `shouldBe` ""
+    let xs :: [Int] = [0, 256, 512, 768, 1024, 1280, 1536, 1792, 2048]
+    binarySearch 513 (xs !!) 2 4 `shouldBe` 2

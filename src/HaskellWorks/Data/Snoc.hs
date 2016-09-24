73c5ecf5e49db7d2f7926842176c1125d30220ba
John Ky <newhoggy@gmail.com>
2016-09-24 18:31:31 +0900

    New Snoc typeclass

--- src/HaskellWorks/Data/Snoc.hs
+++ src/HaskellWorks/Data/Snoc.hs
@@ -18,76 +18,76 @@ class Seq v => Snoc v where
 
 instance Snoc String where
   vSnoc v c = v ++ [c]
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc BS.ByteString where
   vSnoc = BS.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DV.Vector Word8) where
   vSnoc = DV.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DV.Vector Word16) where
   vSnoc = DV.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DV.Vector Word32) where
   vSnoc = DV.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DV.Vector Word64) where
   vSnoc = DV.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DVS.Vector Word8) where
   vSnoc = DVS.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DVS.Vector Word16) where
   vSnoc = DVS.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DVS.Vector Word32) where
   vSnoc = DVS.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DVS.Vector Word64) where
   vSnoc = DVS.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DV.Vector Int8) where
   vSnoc = DV.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DV.Vector Int16) where
   vSnoc = DV.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DV.Vector Int32) where
   vSnoc = DV.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DV.Vector Int64) where
   vSnoc = DV.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DVS.Vector Int8) where
   vSnoc = DVS.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DVS.Vector Int16) where
   vSnoc = DVS.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DVS.Vector Int32) where
   vSnoc = DVS.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DVS.Vector Int64) where
   vSnoc = DVS.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}
 
 instance Snoc (DVS.Vector Int) where
   vSnoc = DVS.snoc
-  {-# INLINE vSnoc     #-}
+  {-# INLINE vSnoc #-}

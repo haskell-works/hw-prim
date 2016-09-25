6cbe342a28aa3cb5f832982dd5b03266ec2eb0ea
John Ky <newhoggy@gmail.com>
2016-09-25 15:16:17 +0800

    Code formatting

--- src/HaskellWorks/Data/AtIndex.hs
+++ src/HaskellWorks/Data/AtIndex.hs
@@ -17,119 +17,119 @@ import           HaskellWorks.Data.Length
 import           HaskellWorks.Data.Positioning
 
 class Length v => AtIndex v where
-  (!!!) :: v -> Position -> Elem v
+  (!!!)   :: v -> Position -> Elem v
   atIndex :: v -> Position -> Elem v
 
 instance AtIndex [a] where
-  (!!!) v i = v !! fromIntegral i
+  (!!!)   v i = v !! fromIntegral i
   atIndex v i = v !! fromIntegral i
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex BS.ByteString where
-  (!!!) v i = v `BS.index` fromIntegral i
+  (!!!)   v i = v `BS.index` fromIntegral i
   atIndex v i = BS.index v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DV.Vector Word8) where
-  (!!!) v i = v DV.! fromIntegral i
+  (!!!)   v i = v DV.! fromIntegral i
   atIndex v i = DV.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DV.Vector Word16) where
-  (!!!) v i = v DV.! fromIntegral i
+  (!!!)   v i = v DV.! fromIntegral i
   atIndex v i = DV.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DV.Vector Word32) where
-  (!!!) v i = v DV.! fromIntegral i
+  (!!!)   v i = v DV.! fromIntegral i
   atIndex v i = DV.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DV.Vector Word64) where
-  (!!!) v i = v DV.! fromIntegral i
+  (!!!)   v i = v DV.! fromIntegral i
   atIndex v i = DV.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DVS.Vector Word8) where
-  (!!!) v i = v DVS.! fromIntegral i
+  (!!!)   v i = v DVS.! fromIntegral i
   atIndex v i = DVS.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DVS.Vector Word16) where
-  (!!!) v i = v DVS.! fromIntegral i
+  (!!!)   v i = v DVS.! fromIntegral i
   atIndex v i = DVS.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DVS.Vector Word32) where
-  (!!!) v i = v DVS.! fromIntegral i
+  (!!!)   v i = v DVS.! fromIntegral i
   atIndex v i = DVS.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DVS.Vector Word64) where
-  (!!!) v i = v DVS.! fromIntegral i
+  (!!!)   v i = v DVS.! fromIntegral i
   atIndex v i = DVS.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DV.Vector Int8) where
-  (!!!) v i = v DV.! fromIntegral i
+  (!!!)   v i = v DV.! fromIntegral i
   atIndex v i = DV.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DV.Vector Int16) where
-  (!!!) v i = v DV.! fromIntegral i
+  (!!!)   v i = v DV.! fromIntegral i
   atIndex v i = DV.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DV.Vector Int32) where
-  (!!!) v i = v DV.! fromIntegral i
+  (!!!)   v i = v DV.! fromIntegral i
   atIndex v i = DV.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DV.Vector Int64) where
-  (!!!) v i = v DV.! fromIntegral i
+  (!!!)   v i = v DV.! fromIntegral i
   atIndex v i = DV.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DVS.Vector Int8) where
-  (!!!) v i = v DVS.! fromIntegral i
+  (!!!)   v i = v DVS.! fromIntegral i
   atIndex v i = DVS.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DVS.Vector Int16) where
-  (!!!) v i = v DVS.! fromIntegral i
+  (!!!)   v i = v DVS.! fromIntegral i
   atIndex v i = DVS.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DVS.Vector Int32) where
-  (!!!) v i = v DVS.! fromIntegral i
+  (!!!)   v i = v DVS.! fromIntegral i
   atIndex v i = DVS.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DVS.Vector Int64) where
-  (!!!) v i = v DVS.! fromIntegral i
+  (!!!)   v i = v DVS.! fromIntegral i
   atIndex v i = DVS.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}
 
 instance AtIndex (DVS.Vector Int) where
-  (!!!) v i = v DVS.! fromIntegral i
+  (!!!)   v i = v DVS.! fromIntegral i
   atIndex v i = DVS.unsafeIndex v (fromIntegral i)
   {-# INLINE (!!!)   #-}
   {-# INLINE atIndex #-}

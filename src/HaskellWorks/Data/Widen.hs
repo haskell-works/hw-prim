ee08081adf1fd1787e7a924e53991beae1119bcf
John Ky <newhoggy@gmail.com>
2016-10-23 15:07:30 +1100

    Move Widen and Narrow type classes to another library

--- src/HaskellWorks/Data/Widen.hs
+++
@@ -1,8 +0,0 @@
-module HaskellWorks.Data.Widen
-  ( module X
-  ) where
-
-import HaskellWorks.Data.Widen.Widen8  as X
-import HaskellWorks.Data.Widen.Widen16 as X
-import HaskellWorks.Data.Widen.Widen32 as X
-import HaskellWorks.Data.Widen.Widen64 as X

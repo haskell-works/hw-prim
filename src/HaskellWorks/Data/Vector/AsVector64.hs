b2a00d3f3dae4a9084908c2475c843c7283eb5ff
John Ky <newhoggy@gmail.com>
2018-07-10 17:37:57 +1000

    Remove bounds-checking for performance

--- src/HaskellWorks/Data/Vector/AsVector64.hs
+++ src/HaskellWorks/Data/Vector/AsVector64.hs
@@ -5,6 +5,7 @@ module HaskellWorks.Data.Vector.AsVector64
 
 import Data.Bits
 import Data.Word
+import HaskellWorks.Data.AtIndex
 
 import qualified Data.ByteString      as BS
 import qualified Data.Vector.Storable as DVS
@@ -18,16 +19,16 @@ instance AsVector64 (DVS.Vector Word64) where
 instance AsVector64 BS.ByteString where
   asVector64 bs = DVS.constructN ((BS.length bs + 7) `div` 8) go
     where go :: DVS.Vector Word64 -> Word64
-          go u = let bsi = DVS.length u * 8 in
-            if bsi + 8 <= BS.length bs
-              then  let w0 = fromIntegral $ BS.index bs (bsi + 0)
-                        w1 = fromIntegral $ BS.index bs (bsi + 1)
-                        w2 = fromIntegral $ BS.index bs (bsi + 2)
-                        w3 = fromIntegral $ BS.index bs (bsi + 3)
-                        w4 = fromIntegral $ BS.index bs (bsi + 4)
-                        w5 = fromIntegral $ BS.index bs (bsi + 5)
-                        w6 = fromIntegral $ BS.index bs (bsi + 6)
-                        w7 = fromIntegral $ BS.index bs (bsi + 7)
+          go u = let bsi = end u * 8 in
+            if bsi + 8 <= end bs
+              then  let w0 = fromIntegral $ bs !!! (bsi + 0)
+                        w1 = fromIntegral $ bs !!! (bsi + 1)
+                        w2 = fromIntegral $ bs !!! (bsi + 2)
+                        w3 = fromIntegral $ bs !!! (bsi + 3)
+                        w4 = fromIntegral $ bs !!! (bsi + 4)
+                        w5 = fromIntegral $ bs !!! (bsi + 5)
+                        w6 = fromIntegral $ bs !!! (bsi + 6)
+                        w7 = fromIntegral $ bs !!! (bsi + 7)
                     in  (w0 `shiftL`  0) .|.
                         (w1 `shiftL`  8) .|.
                         (w2 `shiftL` 16) .|.
@@ -36,14 +37,14 @@ instance AsVector64 BS.ByteString where
                         (w5 `shiftL` 40) .|.
                         (w6 `shiftL` 48) .|.
                         (w7 `shiftL` 56)
-              else  let w0 = let i = bsi + 0 in if i < BS.length bs then fromIntegral $ BS.index bs (bsi + 0) else 0
-                        w1 = let i = bsi + 1 in if i < BS.length bs then fromIntegral $ BS.index bs (bsi + 1) else 0
-                        w2 = let i = bsi + 2 in if i < BS.length bs then fromIntegral $ BS.index bs (bsi + 2) else 0
-                        w3 = let i = bsi + 3 in if i < BS.length bs then fromIntegral $ BS.index bs (bsi + 3) else 0
-                        w4 = let i = bsi + 4 in if i < BS.length bs then fromIntegral $ BS.index bs (bsi + 4) else 0
-                        w5 = let i = bsi + 5 in if i < BS.length bs then fromIntegral $ BS.index bs (bsi + 5) else 0
-                        w6 = let i = bsi + 6 in if i < BS.length bs then fromIntegral $ BS.index bs (bsi + 6) else 0
-                        w7 = let i = bsi + 7 in if i < BS.length bs then fromIntegral $ BS.index bs (bsi + 7) else 0
+              else  let w0 = let i = bsi + 0 in if i < end bs then fromIntegral $ bs !!! (bsi + 0) else 0
+                        w1 = let i = bsi + 1 in if i < end bs then fromIntegral $ bs !!! (bsi + 1) else 0
+                        w2 = let i = bsi + 2 in if i < end bs then fromIntegral $ bs !!! (bsi + 2) else 0
+                        w3 = let i = bsi + 3 in if i < end bs then fromIntegral $ bs !!! (bsi + 3) else 0
+                        w4 = let i = bsi + 4 in if i < end bs then fromIntegral $ bs !!! (bsi + 4) else 0
+                        w5 = let i = bsi + 5 in if i < end bs then fromIntegral $ bs !!! (bsi + 5) else 0
+                        w6 = let i = bsi + 6 in if i < end bs then fromIntegral $ bs !!! (bsi + 6) else 0
+                        w7 = let i = bsi + 7 in if i < end bs then fromIntegral $ bs !!! (bsi + 7) else 0
                     in  (w0 `shiftL`  0) .|.
                         (w1 `shiftL`  8) .|.
                         (w2 `shiftL` 16) .|.

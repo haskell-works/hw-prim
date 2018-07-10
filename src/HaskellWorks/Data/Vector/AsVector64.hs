4372f0390c3b35c1e3cc6c3090a6ff2dafd71f31
John Ky <newhoggy@gmail.com>
2018-07-10 20:57:51 +1000

    Simplify AsVector64 BS.ByteString instance

--- src/HaskellWorks/Data/Vector/AsVector64.hs
+++ src/HaskellWorks/Data/Vector/AsVector64.hs
@@ -1,14 +1,15 @@
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module HaskellWorks.Data.Vector.AsVector64
   ( AsVector64(..)
   ) where
 
-import Data.Bits
 import Data.Word
-import HaskellWorks.Data.AtIndex
 
-import qualified Data.ByteString      as BS
-import qualified Data.Vector.Storable as DVS
+import qualified Data.ByteString        as BS
+import qualified Data.ByteString.Unsafe as BSU
+import qualified Data.Vector.Storable   as DVS
 
 class AsVector64 a where
   asVector64 :: a -> DVS.Vector Word64
@@ -17,40 +18,8 @@ instance AsVector64 (DVS.Vector Word64) where
   asVector64 = id
 
 instance AsVector64 BS.ByteString where
-  asVector64 bs = DVS.constructN ((BS.length bs + 7) `div` 8) go
-    where go :: DVS.Vector Word64 -> Word64
-          go u = let bsi = end u * 8 in
-            if bsi + 8 <= end bs
-              then  let w0 = fromIntegral $ bs !!! (bsi + 0)
-                        w1 = fromIntegral $ bs !!! (bsi + 1)
-                        w2 = fromIntegral $ bs !!! (bsi + 2)
-                        w3 = fromIntegral $ bs !!! (bsi + 3)
-                        w4 = fromIntegral $ bs !!! (bsi + 4)
-                        w5 = fromIntegral $ bs !!! (bsi + 5)
-                        w6 = fromIntegral $ bs !!! (bsi + 6)
-                        w7 = fromIntegral $ bs !!! (bsi + 7)
-                    in  (w0 `shiftL`  0) .|.
-                        (w1 `shiftL`  8) .|.
-                        (w2 `shiftL` 16) .|.
-                        (w3 `shiftL` 24) .|.
-                        (w4 `shiftL` 32) .|.
-                        (w5 `shiftL` 40) .|.
-                        (w6 `shiftL` 48) .|.
-                        (w7 `shiftL` 56)
-              else  let w0 = let i = bsi + 0 in if i < end bs then fromIntegral $ bs !!! (bsi + 0) else 0
-                        w1 = let i = bsi + 1 in if i < end bs then fromIntegral $ bs !!! (bsi + 1) else 0
-                        w2 = let i = bsi + 2 in if i < end bs then fromIntegral $ bs !!! (bsi + 2) else 0
-                        w3 = let i = bsi + 3 in if i < end bs then fromIntegral $ bs !!! (bsi + 3) else 0
-                        w4 = let i = bsi + 4 in if i < end bs then fromIntegral $ bs !!! (bsi + 4) else 0
-                        w5 = let i = bsi + 5 in if i < end bs then fromIntegral $ bs !!! (bsi + 5) else 0
-                        w6 = let i = bsi + 6 in if i < end bs then fromIntegral $ bs !!! (bsi + 6) else 0
-                        w7 = let i = bsi + 7 in if i < end bs then fromIntegral $ bs !!! (bsi + 7) else 0
-                    in  (w0 `shiftL`  0) .|.
-                        (w1 `shiftL`  8) .|.
-                        (w2 `shiftL` 16) .|.
-                        (w3 `shiftL` 24) .|.
-                        (w4 `shiftL` 32) .|.
-                        (w5 `shiftL` 40) .|.
-                        (w6 `shiftL` 48) .|.
-                        (w7 `shiftL` 56)
-
+  asVector64 bs = DVS.unsafeCast v
+    where bsLen = BS.length bs
+          v = DVS.constructN ((BS.length bs + 7) `div` 8) go :: DVS.Vector Word8
+          go :: DVS.Vector Word8 -> Word8
+          go u = let ui = DVS.length u in if ui < bsLen then BSU.unsafeIndex bs ui else 0

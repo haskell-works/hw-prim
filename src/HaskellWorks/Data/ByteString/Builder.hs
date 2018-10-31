73fef8d2665d8f2f362fc164559b87a5c95085d5
John Ky <newhoggy@gmail.com>
2018-10-31 22:23:54 +1100

    New chainInterleaveIO function

---
+++ src/HaskellWorks/Data/ByteString/Builder.hs
@@ -0,0 +1,21 @@
+module HaskellWorks.Data.ByteString.Builder
+  ( chainInterleaveIO
+  ) where
+
+import Data.Function
+import Data.Monoid   (Monoid (..))
+
+import qualified Data.ByteString      as BS
+import qualified Data.ByteString.Lazy as LBS
+import qualified System.IO.Unsafe     as IO
+
+chainInterleaveIO :: [IO LBS.ByteString] -> IO LBS.ByteString
+chainInterleaveIO iobs = LBS.fromChunks . ([] &) <$> go diobs
+  where diobs :: [IO ([BS.ByteString] -> [BS.ByteString])]
+        diobs = ((++) . LBS.toChunks <$>) <$> iobs
+        go :: [IO ([BS.ByteString] -> [BS.ByteString])] -> IO ([BS.ByteString] -> [BS.ByteString])
+        go (ma:mas) = do
+          a <- ma
+          as <- IO.unsafeInterleaveIO $ go mas
+          return (a . as)
+        go [] = return mempty

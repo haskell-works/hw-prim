03e2bb0f9f827ae1b1f2fc80633f0c39714c831a
John Ky <newhoggy@gmail.com>
2019-06-06 23:18:30 +1000

    Cons, Unsnoc and patterns

---
+++ src/HaskellWorks/Data/Ops.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE ViewPatterns    #-}
+
+module HaskellWorks.Data.Ops
+  ( (<|)
+  , (|>)
+  , pattern (:<|)
+  , pattern (:|>)
+  , pattern Empty
+  , (><)
+  ) where
+
+import Data.Semigroup ((<>))
+
+import qualified HaskellWorks.Data.Cons      as HW
+import qualified HaskellWorks.Data.Container as HW
+import qualified HaskellWorks.Data.Empty     as HW
+import qualified HaskellWorks.Data.Null      as HW
+import qualified HaskellWorks.Data.Snoc      as HW
+import qualified HaskellWorks.Data.Uncons    as HW
+import qualified HaskellWorks.Data.Unsnoc    as HW
+
+infixr 5 :<|
+infixr 5 <|
+
+infixl 5 :|>
+infixl 5 |>
+
+(<|) :: HW.Cons v => HW.Elem v -> v -> v
+(<|) = HW.cons
+{-# INLINE (<|) #-}
+
+(|>) :: HW.Snoc v => v -> HW.Elem v -> v
+(|>) = HW.snoc
+{-# INLINE (|>) #-}
+
+(><) :: (Semigroup v, HW.Container v) => v -> v -> v
+(><) = (<>)
+{-# INLINE (><) #-}
+
+pattern (:<|) :: (HW.Cons v, HW.Uncons v) => HW.Elem v -> v -> v
+pattern a :<| b <- (HW.uncons -> Just (a, b)) where
+  a :<| b = HW.cons a b
+
+pattern (:|>) :: (HW.Snoc v, HW.Unsnoc v) => HW.Elem v -> v -> v
+pattern a :|> b <- (HW.unsnoc -> Just (a, b))
+  where a :|> b = HW.snoc b a
+
+pattern Empty :: (HW.Null v, HW.Empty v) => v
+pattern Empty <- (HW.null -> True)
+  where Empty = HW.empty

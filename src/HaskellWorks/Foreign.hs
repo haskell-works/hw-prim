cdef1e70ddfbf16f81e6fd5f0899cb129060d4ae
John Ky <newhoggy@gmail.com>
2019-10-01 21:28:40 +1000

    New mallocForeignPtrBytesWithAlignedCastPtr function

--- src/HaskellWorks/Foreign.hs
+++ src/HaskellWorks/Foreign.hs
@@ -1,5 +1,6 @@
 module HaskellWorks.Foreign
   ( mallocForeignPtrBytesWithAlignedPtr
+  , mallocForeignPtrBytesWithAlignedCastPtr
   ) where
 
 import Foreign.ForeignPtr (ForeignPtr)
@@ -8,9 +9,16 @@ import Foreign.Storable   (Storable)
 
 import qualified Foreign.ForeignPtr        as F
 import qualified Foreign.ForeignPtr.Unsafe as F
+import qualified Foreign.Ptr               as F
 
 mallocForeignPtrBytesWithAlignedPtr :: Storable a => Int -> Int -> IO (ForeignPtr a, Ptr a)
 mallocForeignPtrBytesWithAlignedPtr alignment n = do
   fptr <- F.mallocForeignPtrBytes (n + alignment)
   let alignedPtr = alignPtr (F.unsafeForeignPtrToPtr fptr) alignment
   return (fptr, alignedPtr)
+
+mallocForeignPtrBytesWithAlignedCastPtr :: Storable a => Int -> Int -> IO (ForeignPtr a, Ptr b)
+mallocForeignPtrBytesWithAlignedCastPtr alignment n = do
+  fptr <- F.mallocForeignPtrBytes (n + alignment)
+  let alignedPtr = alignPtr (F.unsafeForeignPtrToPtr fptr) alignment
+  return (fptr, F.castPtr alignedPtr)

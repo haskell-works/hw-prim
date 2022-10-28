1cde9e625a871fdb0d334afaa1a5b277c849a374
Berk Ozkutuk <ozkutuk@protonmail.com>
2022-10-28 17:20:23 +0200

    Add GHC 9.4 support

--- src/HaskellWorks/Data/Word.hs
+++ src/HaskellWorks/Data/Word.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP       #-}
 {-# LANGUAGE MagicHash #-}
 
 module HaskellWorks.Data.Word
@@ -10,5 +11,9 @@ import GHC.Prim
 import GHC.Word  hiding (ltWord64)
 
 ltWord64 :: Word64 -> Word64 -> Word64
+#if MIN_VERSION_base(4,17,0)
+ltWord64 (W64# a#) (W64# b#) = fromIntegral (I# (ltWord64# a# b#))
+#else
 ltWord64 (W64# a#) (W64# b#) = fromIntegral (I64# (ltWord# a# b#))
+#endif
 {-# INLINE ltWord64 #-}
